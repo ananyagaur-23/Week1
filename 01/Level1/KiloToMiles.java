adb7c5754abac63b8f84d03fb0a34a9c39f25fb3
ananyagaur-23 <ananyagaur.23@gmail.com>
2025-05-21 22:26:39 +0530

    Clean old repo

--- 01/Level1/KiloToMiles.java
+++
@@ -1,9 +0,0 @@
-public class KiloToMiles{
-	public static void main(String[] args){
-		
-		double k = 10.8;
-		double m = (k * 1.6);
-		
-		System.out.println("The distance " + k + "km in miles is " + m);		
-	}
-}
--- 01/Level1/Pen.java
+++
@@ -1,9 +0,0 @@
-public class Pen{
-	public static void main(String[] args){
-		
-		int p = 14;
-		int s = 3;
-				
-		System.out.println("The Pen Per Student is "+(p/s)+" and the remaining pen not distributed is "+(p%s));		
-	}
-}
